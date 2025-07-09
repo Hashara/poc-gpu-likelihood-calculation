bc64c2e25a181fb72f609fbc7070369f41b8be96
Hashara <kumarasinghe.k.a.s.h@gmail.com>
2025-07-09 11:02:57 +1000

    add openACC code

---
+++ helper/MatrixOpOpenACC.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Hashara Kumarasinghe on 9/7/2025.
+//
+
+#include "MatrixOpOpenACC.h"
+
+Matrix MatrixOpOpenACC::multiply(const Matrix &A, const Matrix &B) {
+    if (A.cols() != B.rows()) {
+        throw std::invalid_argument("Matrix dimensions do not match for multiplication.");
+    }
+
+    Matrix C(A.rows(), B.cols());
+    #pragma acc parallel loop collapse(2)
+    for (size_t i = 0; i < A.rows(); ++i) {
+        for (size_t j = 0; j < B.cols(); ++j) {
+            double sum = 0.0;
+            for (size_t k = 0; k < A.cols(); ++k) {
+                sum += A.at(i, k) * B.at(k, j);
+            }
+            C.at(i, j) = sum;
+        }
+    }
+    return C;
+}
+
+Matrix MatrixOpOpenACC::hadamard(const Matrix &A, const Matrix &B) {
+    if (A.rows() != B.rows() || A.cols() != B.cols()) {
+        throw std::invalid_argument("Matrix dimensions do not match for Hadamard product.");
+    }
+
+    Matrix C(A.rows(), A.cols());
+    #pragma acc parallel loop collapse(2)
+    for (size_t i = 0; i < A.rows(); ++i) {
+        for (size_t j = 0; j < A.cols(); ++j) {
+            C.at(i, j) = A.at(i, j) * B.at(i, j);
+        }
+    }
+    return C;
+}
